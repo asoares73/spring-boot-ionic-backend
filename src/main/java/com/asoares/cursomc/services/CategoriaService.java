bda50b1d25876b5d49ebbf0ac8d5bbb3edd9a7b2
Alexandre Almeida <asoares73@gmail.com>
2018-12-12 19:59:09 -0200

    Vãlidacao sintatica com Bean Validaton

--- src/main/java/com/asoares/cursomc/services/CategoriaService.java
+++ src/main/java/com/asoares/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.asoares.cursomc.domain.Categoria;
+import com.asoares.cursomc.dto.CategoriaDTO;
 import com.asoares.cursomc.repositories.CategoriaRepository;
 import com.asoares.cursomc.services.exceptions.DataIntegrityException;
 import com.asoares.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 					orderBy);
 			return repo.findAll(pageRequest);
+		}
+		
+		public Categoria fromDTO(CategoriaDTO objDto) {
+			return new Categoria(objDto.getId(), objDto.getNome());
 		}
 }
