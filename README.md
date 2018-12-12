8a457893c03d6593f2f0f74874856c2599357cf2
Alexandre Almeida <asoares73@gmail.com>
2018-12-12 11:50:03 -0200

    Pedido, EstadoPagamento e Pagamento

---
+++ README.md
@@ -0,0 +1,22 @@
+# Estudo Springboot + JPA/Hibernate + REST + Ionic + JWT + S3 + MySQL
+
+[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://nodesource.com/products/nsolid)
+
+Códigos para o aprendizado com uso de tecnologias de desenvolvimento de software. 
+
+> *“Escolha uma ideia. Faça dessa ideia a sua vida. 
+> Pense nela, sonhe com ela, viva pensando nela. 
+> Deixe cérebro, músculos, nervos, todas as partes
+> do seu corpo serem preenchidas com essa ideia. 
+> Esse é o caminho para o sucesso” – Swami Vivekananda, pensador hindu*
+
+Tecnologias:
+
+| Tecnlogias | Descrição |
+| ------ | ------ |
+| Springboot | O Spring Boot é um projeto da Spring que veio para facilitar o processo de configuração e publicação de nossas aplicações. A intenção é ter o seu projeto rodando o mais rápido possível e sem complicação. |
+| JPA/Hibernate | Hibernate é o framework para persistência de dados mais utilizado em projetos Java. JPA é a especificação do Java que dita como os frameworks ORM devem ser implementados. |
+| REST | REST, abreviação de "REpresentational State Transfer", não é uma tecnologia, uma biblioteca, e nem tampouco uma arquitetura, mas sim um modelo a ser utilizado para se projetar arquiteturas de software distribuído, baseadas em comunicação via rede.|
+| Ionic | O JWT é um padrão (RFC-7519) de mercado que define como transmitir e armazenar objetos JSON de forma compacta e segura entre diferentes aplicações. Os dados nele contidos podem ser validados a qualquer momento pois o token é assinado digitalmente. |
+| JWT | O JWT é um padrão (RFC-7519) de mercado que define como transmitir e armazenar objetos JSON de forma compacta e segura entre diferentes aplicações. Os dados nele contidos podem ser validados a qualquer momento pois o token é assinado digitalmente. |
+| S3 | O Amazon Simple Storage Service, conhecido como Amazon S3, é um armazenamento de objetos baseado em web service para armazenar e recuperar qualquer volume de dados. |  
--- src/main/java/com/asoares/cursomc/CursomcApplication.java
+++ src/main/java/com/asoares/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.asoares.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.asoares.cursomc.domain.Cidade;
 import com.asoares.cursomc.domain.Cliente;
 import com.asoares.cursomc.domain.Endereco;
 import com.asoares.cursomc.domain.Estado;
+import com.asoares.cursomc.domain.Pagamento;
+import com.asoares.cursomc.domain.PagamentoComBoleto;
+import com.asoares.cursomc.domain.PagamentoComCartao;
+import com.asoares.cursomc.domain.Pedido;
 import com.asoares.cursomc.domain.Produto;
+import com.asoares.cursomc.domain.enums.EstadoPagamento;
 import com.asoares.cursomc.domain.enums.TipoCliente;
 import com.asoares.cursomc.repositories.CategoriaRepository;
 import com.asoares.cursomc.repositories.CidadeRepository;
 import com.asoares.cursomc.repositories.ClienteRepository;
 import com.asoares.cursomc.repositories.EnderecoRepository;
 import com.asoares.cursomc.repositories.EstadoRepository;
+import com.asoares.cursomc.repositories.PagamentoRepository;
+import com.asoares.cursomc.repositories.PedidoRepository;
 import com.asoares.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -89,5 +103,19 @@ public class CursomcApplication implements CommandLineRunner{
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
+		SimpleDateFormat sdf =new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 =  new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 }
