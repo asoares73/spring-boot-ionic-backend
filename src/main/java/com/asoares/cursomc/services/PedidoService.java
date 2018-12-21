a88d95051d98bcc546256e40c1805cfaafa8f2a9
Alexandre Almeida <asoares73@gmail.com>
2018-12-21 21:10:10 -0200

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/asoares/cursomc/services/PedidoService.java
+++ src/main/java/com/asoares/cursomc/services/PedidoService.java
@@ -3,11 +3,13 @@ package com.asoares.cursomc.services;
 import java.util.Date;
 import java.util.Optional;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.asoares.cursomc.domain.Cliente;
 import com.asoares.cursomc.domain.ItemPedido;
 import com.asoares.cursomc.domain.PagamentoComBoleto;
 import com.asoares.cursomc.domain.Pedido;
@@ -15,59 +17,70 @@ import com.asoares.cursomc.domain.enums.EstadoPagamento;
 import com.asoares.cursomc.repositories.ItemPedidoRepository;
 import com.asoares.cursomc.repositories.PagamentoRepository;
 import com.asoares.cursomc.repositories.PedidoRepository;
+import com.asoares.cursomc.security.UserSS;
+import com.asoares.cursomc.services.exceptions.AuthorizationException;
 import com.asoares.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
-
-		@Autowired
-		private PedidoRepository repo;
-		
-		@Autowired
-		private BoletoService boletoService;
-		
-		@Autowired
-		private PagamentoRepository pagamentoRepository;
-		
-		@Autowired
-		private ItemPedidoRepository itemPedidoRepository;		
-		
-		@Autowired
-		private ProdutoService produtoService;
-
-		@Autowired
-		private ClienteService clienteService;
-		
-		@Autowired
-		private EmailService emailService;
-
-		public Pedido find(Integer id) {
-			Optional <Pedido> obj = repo.findById(id);
-			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id
-					+ ", Tipo: " + Pedido.class.getName()));
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@Autowired
+	private EmailService emailService;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
-		
-		@Transactional
-		public Pedido insert(Pedido obj) {
-			obj.setId(null);
-			obj.setInstante(new Date());
-			obj.setCliente(clienteService.find(obj.getCliente().getId()));
-			obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
-			obj.getPagamento().setPedido(obj);
-			if (obj.getPagamento() instanceof PagamentoComBoleto) {
-				PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
-				boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
-			}
-			obj = repo.save(obj);
-			pagamentoRepository.save(obj.getPagamento());
-			for(ItemPedido ip : obj.getItens()) {
-				ip.setDesconto(0.0);
-				ip.setProduto(produtoService.find(ip.getProduto().getId()));
-				ip.setPreco(ip.getProduto().getPreco());
-				ip.setPedido(obj);
-			}
-			itemPedidoRepository.saveAll(obj.getItens());
-			emailService.sendOrderConfirmationHtmlEmail(obj);
-			return obj;
-		}		
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		emailService.sendOrderConfirmationEmail(obj);
+		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
 }
