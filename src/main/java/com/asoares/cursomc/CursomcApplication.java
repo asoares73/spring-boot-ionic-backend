44a8d091ab90cdccc6d33d5da748d49b83edef45
Alexandre Almeida <asoares73@gmail.com>
2018-12-26 16:07:46 -0200

    Salvando primeiro arquivo no S3

--- src/main/java/com/asoares/cursomc/CursomcApplication.java
+++ src/main/java/com/asoares/cursomc/CursomcApplication.java
@@ -4,12 +4,19 @@ import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.asoares.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
+	
 	@PostConstruct
 	public void init() {
 		// Setting Spring Boot SetTimeZone
@@ -22,6 +29,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("D:\\ana_exemplo.jpg");
 	}
 }
